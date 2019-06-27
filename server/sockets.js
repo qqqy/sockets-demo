81073d491cc9902c84eadf43778d694c961f58d6
qqqy <frogasaurus@hotmail.com>
2019-06-27 14:05:18 -0600

    added a server, didn't work

--- server/sockets.js
+++ server/sockets.js
@@ -1,4 +1,9 @@
-const io = require("socket.io")();
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+
+
+const io = require("socket.io")(server);
 const messages = []
 
 io.on("connection" , (client) => {
