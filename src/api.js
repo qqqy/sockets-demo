e8e1373a93e536b736b75c747ef2c2072a77f01c
qqqy <frogasaurus@hotmail.com>
2019-06-28 11:58:53 -0600

    just wrapping up

--- src/api.js
+++ src/api.js
@@ -1,8 +1,6 @@
 import openSocket from 'socket.io-client';
 const socket = openSocket("http://192.168.1.8:8000");
 
-socket.on("emittedMessage" , () => console.log("We got it!"))
-
 function subscribeToTimer(cb){
   socket.emit("subscribeToTimer" , 1000);
   socket.on("timer" , timestamp => cb(null , timestamp));
